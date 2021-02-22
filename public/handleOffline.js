5762191a1cc20c6404c404902a033dca71285d68
Logan Zehr <zehrl315@outlook.com>
2021-02-22 15:19:26 -0800

    request.onsuccess checks if browser is online.

--- public/handleOffline.js
+++ public/handleOffline.js
@@ -3,16 +3,25 @@ let db;
 const request = indexedDB.open('budgetIDDB', 1);
 
 // Create new object store if needed
-request.onupgradeneeded = function(event) {
+request.onupgradeneeded = function (event) {
     db = event.target.result;
     console.log("onupgradeneeded db: ", db);
-    db.createObjectStore("pending", {autoIncrement: true});
+    db.createObjectStore("pending", { autoIncrement: true });
 }
 
-// Check if browser is online
+// Check if browser is online initially
+request.onsuccess = function (event) {
+    db = event.target.result;
+    if (navigator.onLine) {
+        console.log("request.onsuccess => navigator.onLine: ", navigator.onLine)
+    }
+}
 
 // Check IndexedDB for records and add to MongoDB if online
 
 // If error, print error
 
 // Save record in budget IndexedDB
+
+// listen for when browser comes online
+// window.addEventListener("online", checkDatabase);
