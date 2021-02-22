ab7e6e91231e1fbb191300fd461133554f7c7dfa
Logan Zehr <zehrl315@outlook.com>
2021-02-22 15:11:34 -0800

    indexed db budget request and object store

--- public/handleOffline.js
+++ public/handleOffline.js
@@ -1,6 +1,13 @@
 // Create new db request for "budget" IndexedDB
+let db;
+const request = indexedDB.open('budgetIDDB', 1);
 
 // Create new object store if needed
+request.onupgradeneeded = function(event) {
+    db = event.target.result;
+    console.log("onupgradeneeded db: ", db);
+    db.createObjectStore("pending", {autoIncrement: true});
+}
 
 // Check if browser is online
 
